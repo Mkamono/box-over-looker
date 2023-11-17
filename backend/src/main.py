6ec17c04a9dc24053abad858b8945e2b49ba3bfe
kakerururu <144202943+kakerururu@users.noreply.github.com>
2023-11-18 01:33:21 +0900

    sleepの導入とアーキテクチャ指定 (#13)

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,6 @@
 import os
 from multiprocessing import Process
-
+from time import sleep
 from migrate import migrate_db
 from scheduler import exec_regularly
 from server import run_server
@@ -18,5 +18,6 @@ def run_parallel_process() -> None:
 
 
 if __name__ == "__main__":
+    sleep(5)
     migrate_db(db_name=os.environ["POSTGRES_DB"])
     run_parallel_process()
