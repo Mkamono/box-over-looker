b3be2c515eac44e36c2d4c998d4d5df620efe5d4
kakerururu <beatkakeru@icloud.com>
2023-11-24 11:57:40 +0000

    ✨ feat: POST内容をメール関数に渡して送信するエンドポイントの作成

--- mail/src/main.py
+++ mail/src/main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from send_mail import send_email
 
 app = Flask(__name__)
 
@@ -6,3 +7,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world() -> str:
     return "<p>Hello, World!</p>"
+
+
+@app.route("/send_mail", methods=["POST"])
+def post() -> str:
+    mail: dict = request.get_json()
+    send_email(mail["title"], mail["body"], mail["user_address"])
+    return "OK"
