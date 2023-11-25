e9769021ee112b6e55adac49432fdbd6b98583db
kakerururu <beatkakeru@icloud.com>
2023-11-25 15:27:32 +0000

    🦄 refactor: 文字列returnの修正＆if文のエラーキャッチを削除

--- mail/src/main.py
+++ mail/src/main.py
@@ -1,7 +1,6 @@
 from smtplib import SMTPRecipientsRefused
 
 from flask import Flask, request
-from requests.exceptions import InvalidURL, MissingSchema
 from send_mail import send_email
 
 app = Flask(__name__)
@@ -15,14 +14,10 @@ def hello_world() -> str:
 @app.route("/send_mail", methods=["POST"])
 def post() -> str:
     mail: dict = request.get_json()
-    if mail is None:
-        raise MissingSchema("Request body is missing")
-    if not all(key in mail for key in ["title", "body", "user_address"]):
-        raise InvalidURL("Request body is invalid")
     try:
         send_email(mail["title"], mail["body"], mail["user_address"])
     except SMTPRecipientsRefused as e:
-        return "Invalid email address"
+        raise e
     except Exception as e:
-        return str(e)
+        raise e
     return "OK"
