9917e819eb20dcd41538656467dcc320bd1e6b25
kakerururu <beatkakeru@icloud.com>
2023-11-27 15:36:19 +0000

    Refactor retry functions in tools.py to handle GET requests properly

--- scraping/src/domain/tools.py
+++ scraping/src/domain/tools.py
@@ -1,16 +1,34 @@
 from time import sleep
 
-from errors import ItemNotFoundError
+from errors import IncorrectGetError, ItemNotFoundError
 
 
-def retry(func):
+def retry_get_element(func):
     def wrapper(*args, **kwargs):
-        while True:
+        for i in range(10):
             try:
                 return func(*args, **kwargs)
             except ItemNotFoundError as e:
                 raise e
             except:
                 sleep(1)
+        raise IncorrectGetError
+
+    return wrapper
+
+
+def retry_get_request(func):
+    def wrapper(*args, **kwargs):
+        for i in range(3):
+            try:
+                return func(*args, **kwargs)
+            except IncorrectGetError:
+                print("GETリクエストに失敗しました。リトライします。")
+                continue
+            except Exception as e:
+                print("GETリクエストに予期せぬエラーが発生しました。")
+                print(e)
+                raise e
+        raise ItemNotFoundError
 
     return wrapper
