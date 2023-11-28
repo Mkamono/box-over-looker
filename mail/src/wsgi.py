5a3f7a606f1dd9639f3aefcb9518285ed5d47911
kakerururu <144202943+kakerururu@users.noreply.github.com>
2023-11-29 00:29:19 +0900

    wsgiファイルの実装 (#31)

---
+++ mail/src/wsgi.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+
+project_folder: str = "/home/kakerururu/box-over-looker"
+load_dotenv(os.path.join(project_folder, ".env"))
+
+path: str = f"{project_folder}/mail/src"
+if path not in sys.path:
+    sys.path.append(path)
+
+# 起動時に読み込むファイルをmain.pyに指定
+from main import app as application
