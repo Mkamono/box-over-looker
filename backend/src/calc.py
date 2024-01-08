1a8d6fa81e27234252d46a694d240506256fe495
kakerururu <beatkakeru@icloud.com>
2024-01-08 09:08:41 +0000

    🦄 refactor: 中央値算出関数を省略＆変数名の軽微な修正

--- backend/src/calc.py
+++ backend/src/calc.py
@@ -3,7 +3,7 @@ import statistics
 from models import Analysis, Product, ScrapingResults
 
 
-def make_analysis_list_every_product(
+def make_analysis_list(
     scraping_results: ScrapingResults,
 ) -> list[Analysis]:
     scraping_datetime = scraping_results.scraping_results[0].date
@@ -11,15 +11,19 @@ def make_analysis_list_every_product(
     def calc_price_median_by_product(
         product: Product, scraping_results: ScrapingResults
     ) -> float:
-        return calc_price_median(
-            ScrapingResults(
-                scraping_results=[
-                    item
-                    for item in scraping_results.scraping_results
-                    if item.product == product
-                ]
-            )
+        items = ScrapingResults(
+            scraping_results=[
+                item
+                for item in scraping_results.scraping_results
+                if item.product == product
+            ]
         )
+
+        prices: list[float] = [
+            item.price for item in items.scraping_results if item.price is not None
+        ]
+        median = statistics.median(prices)
+        return median
 
     analysis_list_every_product = [
         Analysis(
@@ -33,9 +37,9 @@ def make_analysis_list_every_product(
     return analysis_list_every_product
 
 
-def calc_price_median(Items: ScrapingResults) -> float:
+def calc_price_median(items: ScrapingResults) -> float:
     price_list: list[float] = [
-        item.price for item in Items.scraping_results if item.price is not None
+        item.price for item in items.scraping_results if item.price is not None
     ]
     median = statistics.median(price_list)
     return median
