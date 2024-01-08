c0b82f3b7bcfef783c28928d620540115b22fcc2
kakerururu <beatkakeru@icloud.com>
2024-01-08 08:23:13 +0000

    🦄 refactor: 大量リファクタリング

--- backend/src/calc.py
+++ backend/src/calc.py
@@ -1,36 +1,29 @@
-from statistics import median
+import statistics
 
 from models import Analysis, Product, ScrapingResults
 
 
-def create_median_list_every_product(
-    single_scraping_results: ScrapingResults,
+def make_median_list_every_product(
+    single_exec_scraping_results: ScrapingResults,
 ) -> list[Analysis]:
     """
-    スクレイピング一回収集分のデータから各商品ごとの中央値を計算し、リストにして返す。
+    スクレイピング一回収集分の商品データから各商品ごとの中央値を計算して、リスト形式で返します。
 
-    ----------
-    scraping_date: datetime
-        データ収集日時。データの１つ目の日時を代表値とし、分、秒、マイクロ秒は切り捨てる。
-    ----------
+    Parameters:
+    single_exec_scraping_results (ScrapingResults): 一回分のスクレイピングによる商品データ
 
+    Returns:
+    list[Analysis]: 各製品の価格中央値を含むAnalysisオブジェクトのリスト。
     """
-    scraping_date = single_scraping_results.scraping_results[0].date.replace(
+
+    scraping_datetime = single_exec_scraping_results.scraping_results[0].date.replace(
         minute=0, second=0, microsecond=0
     )
-    median_list_every_product: list[Analysis] = [
+    median_list_every_product = [
         Analysis(
-            date=scraping_date,
+            date=scraping_datetime,
             product=product,
-            median=calc_median(
-                ScrapingResults(
-                    scraping_results=[
-                        item
-                        for item in single_scraping_results.scraping_results
-                        if item.product == product
-                    ]
-                )
-            ),
+            median=calc_price_median_by_product(product, single_exec_scraping_results),
         )
         for product in Product
     ]
@@ -38,12 +31,23 @@ def create_median_list_every_product(
     return median_list_every_product
 
 
-def calc_median(Items: ScrapingResults) -> float:
+def calc_price_median_by_product(
+    product: Product, single_exec_scraping_results: ScrapingResults
+) -> float:
+    return calc_price_median(
+        ScrapingResults(
+            scraping_results=[
+                item
+                for item in single_exec_scraping_results.scraping_results
+                if item.product == product
+            ]
+        )
+    )
+
+
+def calc_price_median(Items: ScrapingResults) -> float:
     price_list: list[float] = [
         item.price for item in Items.scraping_results if item.price is not None
     ]
-    if len(price_list) == 0:
-        raise ValueError("scraping_result's value is empty")
-
-    median_value = median(price_list)
-    return median_value
+    median = statistics.median(price_list)
+    return median
