94db3f45442d899e60a58adc183c7134d629e73c
kakerururu <beatkakeru@icloud.com>
2024-01-11 14:20:51 +0000

    ✨ feat: Notificationクラスの初期値設定関数

--- backend/src/env_config.py
+++ backend/src/env_config.py
@@ -15,3 +15,20 @@ class EnvConfig(BaseModel):
     threshold_increase_rate_price: float
     notification_timing: NotificationTiming
     period_days: int
+
+
+def get_notification_timing() -> NotificationTiming:
+    def to_bool(value: str) -> bool:
+        if value == "True":
+            return True
+        elif value == "False":
+            return False
+        else:
+            raise ValueError(f"{value} is not True or False")
+
+    notification_timing = NotificationTiming()
+    notification_timing.zero = to_bool(os.environ["NOTIFICATION_TIMING_ZERO"])
+    notification_timing.six = to_bool(os.environ["NOTIFICATION_TIMING_SIX"])
+    notification_timing.twelve = to_bool(os.environ["NOTIFICATION_TIMING_TWELVE"])
+    notification_timing.eighteen = to_bool(os.environ["NOTIFICATION_TIMING_EIGHTEEN"])
+    return notification_timing
