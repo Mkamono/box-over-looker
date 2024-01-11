63e3de9926905cefcde439075f4721d554c6e49d
kakerururu <beatkakeru@icloud.com>
2024-01-11 15:37:04 +0000

    🦄 refactor: convet関数を削除

--- backend/src/env_config.py
+++ backend/src/env_config.py
@@ -11,17 +11,10 @@ class NotificationTiming(BaseModel):
 
 
 def get_notification_timing() -> NotificationTiming:
-    def convert_to_bool(string: str) -> bool:
-        if string == "True":
-            return True
-        elif string == "False":
-            return False
-        else:
-            raise ValueError(f"Invalid value: {string}")
-
     notification_timing = NotificationTiming()
+
     for timing in notification_timing.model_dump():
         notification_timing.__setattr__(
-            timing, convert_to_bool(os.environ[f"NOTIFICATION_TIMING_{timing.upper()}"])
+            timing, os.environ[f"NOTIFICATION_TIMING_{timing.upper()}"]
         )
     return notification_timing
