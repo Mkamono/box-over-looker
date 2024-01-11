fa17a0ca9fd643e6f769144c2f018519462b4905
kakerururu <beatkakeru@icloud.com>
2024-01-11 16:03:40 +0000

    🦄 refactor: ループ処理に変える前に戻す修正

--- backend/src/env_config.py
+++ backend/src/env_config.py
@@ -13,8 +13,20 @@ class NotificationTiming(BaseModel):
 def get_notification_timing() -> NotificationTiming:
     notification_timing = NotificationTiming()
 
-    for timing in notification_timing.model_dump():
-        notification_timing.__setattr__(
-            timing, os.environ[f"NOTIFICATION_TIMING_{timing.upper()}"]
-        )
+    def convert_to_bool(string: str) -> bool:
+        if string == "True":
+            return True
+        elif string == "False":
+            return False
+        else:
+            raise ValueError(f"Invalid value: {string}")
+
+    notification_timing.zero = convert_to_bool(os.environ["NOTIFICATION_TIMING_ZERO"])
+    notification_timing.six = convert_to_bool(os.environ["NOTIFICATION_TIMING_SIX"])
+    notification_timing.twelve = convert_to_bool(
+        os.environ["NOTIFICATION_TIMING_TWELVE"]
+    )
+    notification_timing.eighteen = convert_to_bool(
+        os.environ["NOTIFICATION_TIMING_EIGHTEEN"]
+    )
     return notification_timing
