6887c892d6e745a96efcc14b818d91450c464280
kakerururu <beatkakeru@icloud.com>
2024-01-12 15:44:43 +0000

    🦄 refactor: クラス名を変更

--- backend/src/mail.py
+++ backend/src/mail.py
@@ -2,7 +2,7 @@ from models import Product
 from pydantic import BaseModel
 
 
-class ComparedProduct(BaseModel):
+class ComparedResult(BaseModel):
     product: Product
     current_price: float
     result: bool
