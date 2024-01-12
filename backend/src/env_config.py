fe7740765aa274d36da94c053c08a152412ddf83
kakerururu <beatkakeru@icloud.com>
2024-01-12 13:58:34 +0000

    ✨ feat: env_configの実装

--- backend/src/env_config.py
+++ backend/src/env_config.py
@@ -1,32 +1,31 @@
-import os
+import re
+from pathlib import Path
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class NotificationTiming(BaseModel):
-    zero: bool = False
-    six: bool = False
-    twelve: bool = False
-    eighteen: bool = False
+    zero: bool = Field(alias="0時")
+    six: bool = Field(alias="6時")
+    twelve: bool = Field(alias="12時")
+    eighteen: bool = Field(alias="18時")
 
 
-def get_notification_timing() -> NotificationTiming:
-    notification_timing = NotificationTiming()
+class EnvConfig(BaseModel):
+    mail: str = Field(alias="メールアドレス")
+    threshold_increase_rate_price: float = Field(alias="価格の増加倍率(%)")
+    notification_timing: NotificationTiming = Field(alias="通知時間")
+    period_days: int = Field(alias="価格の過去データの日数")
 
-    def convert_to_bool(string: str) -> bool:
-        if string == "True":
-            return True
-        elif string == "False":
-            return False
-        else:
-            raise ValueError(f"Invalid value: {string}")
 
-    notification_timing.zero = convert_to_bool(os.environ["NOTIFICATION_TIMING_ZERO"])
-    notification_timing.six = convert_to_bool(os.environ["NOTIFICATION_TIMING_SIX"])
-    notification_timing.twelve = convert_to_bool(
-        os.environ["NOTIFICATION_TIMING_TWELVE"]
-    )
-    notification_timing.eighteen = convert_to_bool(
-        os.environ["NOTIFICATION_TIMING_EIGHTEEN"]
-    )
-    return notification_timing
+def get_env_config() -> EnvConfig:
+    user_config_path = Path(__file__).parent.parent.parent / "user_config.jsonc"
+    with open(
+        user_config_path,
+        "r",
+        encoding="utf-8",
+    ) as f:
+        env_config_str = f.read()
+        # jsoncのコメントを削除
+        env_config_re = re.sub(r"/\*[\s\S]*?\*/|//.*", "", env_config_str)
+        return EnvConfig.model_validate_json(env_config_re)
