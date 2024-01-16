024cf61277aacfd562e822a45cf3cd51f6a62d70
kakerururu <beatkakeru@icloud.com>
2024-01-16 08:47:44 +0000

    ✨ feat: メール本文作成関数

--- backend/src/mail.py
+++ backend/src/mail.py
@@ -67,3 +67,62 @@ def make_compared_result_list() -> list[ComparedResult]:
         make_compared_result(product, db_name, user_config) for product in Product
     ]
     return compared_result_list
+
+
+def make_mail_body(compared_results: list[ComparedResult]) -> str:
+    user_config = get_user_config()
+    # 一番初めの文を作成。これに追加していく。
+    body: str = "本日の0時時点での取引価格によるお知らせです。"
+
+    def make_body_by_product(compared_result: ComparedResult) -> str:
+        if compared_result.is_exceed_thd:
+            price_change_word: str = "上昇"
+            word_exceed_thd: str = "急激な価格の上昇が起きています。"
+        if compared_result.increase_price_percentage > 0:
+            price_change_word: str = "上昇"
+            word_exceed_thd: str = ""
+        else:
+            price_change_word: str = "減少"
+            word_exceed_thd: str = ""
+
+        # 1つの商品分の本文を作成する
+        body = f"・{compared_result.product}。\n現在の取引価格は約{compared_result.current_price}円です。{word_exceed_thd}過去{user_config.period_days}日間の平均価格と比較して、{compared_result.increase_price_percentage}%{price_change_word}しています。\n"
+
+        return body
+
+    results_exceed_thd: list[ComparedResult] = [
+        compared_result
+        for compared_result in compared_results
+        if compared_result.is_exceed_thd
+    ]
+
+    results_not_exceed_thd: list[ComparedResult] = [
+        compared_result
+        for compared_result in compared_results
+        if not compared_result.is_exceed_thd
+    ]
+    # 閾値を3つとも超えているかどうかで、本文内のタイトルを変更する。
+    if len(results_exceed_thd) == 0:
+        title_exceed_thd: str = ""
+        title_not_exceed_thd: str = "価格の急激な上昇が確認されなかった商品"
+    if len(results_exceed_thd) == 3:
+        title_exceed_thd: str = "価格の急激な上昇が確認された商品"
+        title_not_exceed_thd: str = ""
+    else:
+        title_exceed_thd: str = "価格の急激な上昇が確認された商品"
+        title_not_exceed_thd: str = "価格の急激な上昇が確認されなかった商品"
+
+    body += f"""
+{title_exceed_thd}
+
+{"".join([make_body_by_product(result) for result in results_exceed_thd])}
+{title_not_exceed_thd}
+
+{"".join([make_body_by_product(result) for result in results_not_exceed_thd])}
+
+現在の閾値は{user_config.threshold_increase_rate_price}%です。閾値の変更は、以下のURLのReadmeをお読みください。
+
+“https://github.com/Mkamono/box-over-looker”
+"""
+    # URLはリポジトリ譲渡後に変更する。
+    return body
