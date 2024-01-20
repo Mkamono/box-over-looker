064003d744c245f85462e139626c98394a47f1c0
kakerururu <144202943+kakerururu@users.noreply.github.com>
2024-01-20 23:30:49 +0900

    ユーザー設定を動的に読み込みメールを送信する関数の実装(#55)

--- backend/src/scheduler.py
+++ backend/src/scheduler.py
@@ -1,23 +1,51 @@
 import os
+from datetime import datetime
 from time import sleep
 
 import requests
 import schedule
-from db import create_item_records
+from calc import make_analysis_list
+from db import create_analysis_records, create_item_records
+from mail import make_compared_result_list, make_mail_class
 from models import ScrapingResults
+from user_config import get_user_config
 
 
-def create_scraping_results() -> None:
+def create_item_and_analysis_records() -> None:
     response = requests.get(
         f"http://{os.environ['SCRAPING_HOST']}:{os.environ['SCRAPING_PORT']}/scraping"
     )
     scraping_results = ScrapingResults(**response.json())
     create_item_records(db_name=os.environ["POSTGRES_DB"], results=scraping_results)
-    return
+    create_analysis_records(
+        db_name=os.environ["POSTGRES_DB"], analysis=make_analysis_list(scraping_results)
+    )
+
+
+def send_mail() -> None:
+    compared_results = make_compared_result_list()
+    mail = make_mail_class(compared_results)
+    mail.post_mail()
+
+
+def send_mail_based_user_config() -> None:
+    user_config = get_user_config()
+    current_hour = datetime.now().hour
+
+    if user_config.notification_timing.zero & current_hour == 0:
+        send_mail()
+    if user_config.notification_timing.six & current_hour == 6:
+        send_mail()
+    if user_config.notification_timing.twelve & current_hour == 12:
+        send_mail()
+    if user_config.notification_timing.eighteen & current_hour == 18:
+        send_mail()
 
 
 def exec_regularly() -> None:
-    schedule.every().hour.at(":00").do(create_scraping_results)
+    schedule.every().hour.at(":00").do(create_item_and_analysis_records)
+    schedule.every().hour.at(":00").do(send_mail_based_user_config)
+
     while True:
         schedule.run_pending()
         sleep(1 * 60)
